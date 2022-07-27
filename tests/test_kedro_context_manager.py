34fba00304193c2dfa9fe1ec6675a80b28043448
marrrcin <m.zablo@gmail.com>
2022-07-27 12:23:22 +0200

    Tests working directory fix

--- tests/test_kedro_context_manager.py
+++ tests/test_kedro_context_manager.py
@@ -6,13 +6,12 @@ from kedro_azureml.config import KedroAzureMLConfig
 from kedro_azureml.utils import KedroContextManager
 
 
-def test_can_create_context_manager():
-    with patch("kedro.framework.project.PACKAGE_NAME", "tests"):
-        with KedroContextManager("tests", "base") as mgr:
-            assert mgr is not None and isinstance(
-                mgr, KedroContextManager
-            ), "Invalid object returned"
-            assert isinstance(mgr.context, KedroContext), "No KedroContext"
-            assert isinstance(
-                mgr.plugin_config, KedroAzureMLConfig
-            ), "Invalid plugin config"
+def test_can_create_context_manager(patched_kedro_package):
+    with KedroContextManager("tests", "base") as mgr:
+        assert mgr is not None and isinstance(
+            mgr, KedroContextManager
+        ), "Invalid object returned"
+        assert isinstance(mgr.context, KedroContext), "No KedroContext"
+        assert isinstance(
+            mgr.plugin_config, KedroAzureMLConfig
+        ), "Invalid plugin config"
