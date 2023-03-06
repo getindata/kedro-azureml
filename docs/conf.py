3eeccd135b8c8ef5ccece2217ee35503865b8cc2
Elior Cohen <elior@datascience.co.il>
2023-03-06 13:25:58 +0200

    :memo: Autodoc mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+autodoc_mock_imports = ["azureml", "pandas"]
 
 # -- Options for HTML output -------------------------------------------------
 
