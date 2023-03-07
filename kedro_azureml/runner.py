d00c60a08779a407a75ac1036e977427936d2a84
Tomas Van Pottelbergh <github@tomasvanpottelbergh.com>
2023-03-07 20:55:54 +0100

    Handle AzureMLFolderDatasets from catalog in runner

--- kedro_azureml/runner.py
+++ kedro_azureml/runner.py
@@ -46,10 +46,18 @@ class AzurePipelinesRunner(SequentialRunner):
         hook_manager: PluginManager = None,
         session_id: str = None,
     ) -> Dict[str, Any]:
-        unsatisfied = pipeline.inputs() - set(catalog.list())
-        for ds_name in unsatisfied:
-            catalog = catalog.shallow_copy()
-            catalog.add(ds_name, self.create_default_data_set(ds_name))
+        catalog = catalog.shallow_copy()
+        catalog_set = set(catalog.list())
+
+        # Loop over datasets in arguments to set their paths
+        for ds_name, ds_path in self.data_paths.items():
+            if ds_name in catalog_set:
+                ds = catalog._get_dataset(ds_name)
+                if isinstance(ds, AzureMLFolderDataset):
+                    ds.path = Path(ds_path) / Path(ds._filepath).name
+                    catalog.add(ds_name, ds, replace=True)
+            else:
+                catalog.add(ds_name, self.create_default_data_set(ds_name))
 
         return super().run(pipeline, catalog, hook_manager, session_id)
 
