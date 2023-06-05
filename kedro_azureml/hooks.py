39206f3015db23e9d8f31dea896a23a61698efab
Florian Roessler <roessler.fd@gmail.com>
2023-06-05 21:44:32 +0000

    update hook implementation with versioning

--- kedro_azureml/hooks.py
+++ kedro_azureml/hooks.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 
 from azureml.fsspec import AzureMachineLearningFileSystem
 from kedro.framework.hooks import hook_impl
@@ -7,9 +7,18 @@ from kedro_azureml.config import AzureMLConfig
 from kedro_azureml.client import _get_azureml_client
 from kedro_azureml.datasets.folder_dataset import AzureMLFolderDataSet
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class TooManyFilesError(Exception):
     pass
+
+def get_versioned_path(filepath: PurePosixPath, version: str) -> PurePosixPath:
+        if filepath.is_dir():
+            return filepath / version / filepath.parts[-1]
+        else:
+            return filepath / version / filepath.name
 
 
 class AzureMLLocalRunHook:
@@ -34,9 +43,19 @@ class AzureMLLocalRunHook:
                     if isinstance(dataset, AzureMLFolderDataSet) and (
                         dataset_name in pipeline.inputs()
                     ):
-                        azure_ds = ml_client.data.get(
-                            dataset._azureml_dataset, label="latest"
-                        )
+                        version = "latest" if dataset._version is None else str(dataset._version)
+                        if version == "latest":
+                            azure_ds = ml_client.data.get(
+                                dataset._azureml_dataset, label="latest"
+                            )
+                            # in case of latest there might be files already so
+                            # we definitly want to overwrite on download
+                            overwrite_mode = "MERGE_WITH_OVERWRITE"
+                        else:
+                            azure_ds = ml_client.data.get(
+                                dataset._azureml_dataset, version=dataset._version
+                            )
+                            overwrite_mode = "APPEND"
                         if azure_ds.type == "uri_file":
                             # azure_path = azure_ds.path.rsplit("/", 1)[0]
                             # fs = AzureMachineLearningFileSystem(azure_path)
@@ -48,23 +67,24 @@ class AzureMLLocalRunHook:
                             fs = AzureMachineLearningFileSystem(azure_path)
                         # The datasets filepath is always absolute due to AbstractDataset
                         ds_local_absolute_fpath = Path(dataset._filepath)
-                        # TODO: Figure out the best path structure depending on versioning implementation.
-                        project_path = Path(run_params["project_path"])
-                        new_base_path = project_path / "data" / "local_run"
-                        # I thought about adding `data/` to it and make it relative but that assumes
-                        # that everyone uses a `data/` directory which might not be the case.
-                        ds_local_relative_fpath = ds_local_absolute_fpath.relative_to(
-                            project_path
-                        )
-                        new_filepath = (
-                            new_base_path
-                            / str(azure_ds.version)
-                            / ds_local_relative_fpath
-                        )
+                        new_filepath = get_versioned_path(ds_local_absolute_fpath, version)
+                        # if the path is a file we'll take the parent directory to download into
+                        download_path = new_filepath.parent if ("." in new_filepath.name) else new_filepath
                         # using APPEND will keep the local file if exists
                         # as versions are unique this will prevent unnecessary file download
                         for fpath in fs.ls():
-                            fs.download(fpath, str(new_filepath.parent), overwrite="APPEND")
+                            logger.info(f"Downloading {fpath} for local execution")
+                            fs.download(fpath, str(download_path), overwrite=overwrite_mode)
+                        dataset.path = str(new_filepath)
+                        catalog.add(dataset_name, dataset, replace=True)
+
+                    # we are adding this so that if an intermediate dataset in one
+                    # run becomes the root dataset in another run we don't get problems
+                    # with files being folder in the kedro verion way.
+                    elif isinstance(dataset, AzureMLFolderDataSet) and (
+                        dataset_name not in pipeline.inputs()
+                    ):
+                        new_filepath = get_versioned_path(Path(dataset._filepath), "local")
                         dataset.path = str(new_filepath)
                         catalog.add(dataset_name, dataset, replace=True)
 
