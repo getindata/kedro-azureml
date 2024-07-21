3770e7516b5312efa64a6b03ada913c2ada892c7
kd <kinjal_dand@yahoo.in>
2024-07-21 21:32:10 +0530

    added setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.8.0.1',
     packages=find_packages(),
     entry_points={
+        'kedro.hooks': [
+            'azure_local_run_hook = kedro_azureml.hooks:azureml_local_run_hook',
+        ],
         'kedro.project_commands': [
             'azureml = kedro_azureml.cli:commands',
         ],
