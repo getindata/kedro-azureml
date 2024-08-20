dd86343741c7c58c45aa53ca46e5760e73840340
KD <kinjal_dand@yahoo.in>
2024-08-20 09:26:02 +0530

    enabled msi on AML CI

--- kedro_azureml/auth/utils.py
+++ kedro_azureml/auth/utils.py
@@ -8,10 +8,11 @@ def get_azureml_credentials():
         # On a AzureML compute instance, the managed identity will take precedence,
         # while it does not have enough permissions.
         # So, if we are on an AzureML compute instance, we disable the managed identity.
-        is_azureml_managed_identity = "MSI_ENDPOINT" in os.environ
-        credential = DefaultAzureCredential(
-            exclude_managed_identity_credential=is_azureml_managed_identity
-        )
+        # is_azureml_managed_identity = "MSI_ENDPOINT" in os.environ
+        # credential = DefaultAzureCredential(
+        #     exclude_managed_identity_credential=is_azureml_managed_identity
+        # )
+        credential = DefaultAzureCredential()
         # Check if given credential can get token successfully.
         credential.get_token("https://management.azure.com/.default")
     except Exception:
