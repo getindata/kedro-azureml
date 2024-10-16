922848b1ebaf270f8901302fc5ee18a0dbdd79fc
AlexandreOuellet <Alexandre.Ouellet.1@gmail.com>
2024-10-16 16:06:30 -0400

    remove checks for catalog list, and use catalog.__contains__() instead
    catalog already has a __contains__ method, which ensures that the dataset name (or input/output name in some cases) fits either with a known dataset, or a dataset pattern, which enables it to work with dataset factory

--- kedro_azureml/generator.py
+++ kedro_azureml/generator.py
@@ -157,7 +157,7 @@ class AzureMLPipelineGenerator:
     def _get_input(self, dataset_name: str, pipeline: Pipeline) -> Input:
         if self._is_param_or_root_non_azureml_asset_dataset(dataset_name, pipeline):
             return Input(type="string")
-        elif dataset_name in self.catalog.list() and isinstance(
+        elif dataset_name in self.catalog and isinstance(
             ds := self.catalog._get_dataset(dataset_name), AzureMLAssetDataset
         ):
             if ds._azureml_type == "uri_file" and dataset_name not in pipeline.inputs():
@@ -169,7 +169,7 @@ class AzureMLPipelineGenerator:
             return Input(type="uri_folder")
 
     def _get_output(self, name):
-        if name in self.catalog.list() and isinstance(
+        if name in self.catalog and isinstance(
             ds := self.catalog._get_dataset(name), AzureMLAssetDataset
         ):
             if ds._azureml_type == "uri_file":
@@ -208,7 +208,7 @@ class AzureMLPipelineGenerator:
     ) -> bool:
         return dataset_name.startswith(PARAMS_PREFIX) or (
             dataset_name in pipeline.inputs()
-            and dataset_name in self.catalog.list()
+            and dataset_name in self.catalog
             and not isinstance(
                 self.catalog._get_dataset(dataset_name), AzureMLAssetDataset
             )
@@ -334,7 +334,7 @@ class AzureMLPipelineGenerator:
                     azure_output = parent_outputs[sanitized_input_name]
                     azure_inputs[sanitized_input_name] = azure_output
                 # 2. try to find AzureMLAssetDataset in catalog
-                elif node_input in self.catalog.list() and isinstance(
+                elif node_input in self.catalog and isinstance(
                     ds := self.catalog._get_dataset(node_input), AzureMLAssetDataset
                 ):
                     azure_inputs[sanitized_input_name] = Input(
