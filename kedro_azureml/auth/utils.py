a1e8b28655332bdc1f3cc89114f6add08f08f481
Mateusz Pytel <mateusz.pytel@xebia.com>
2025-08-13 04:09:10 +0200

    fix tests

--- kedro_azureml/auth/utils.py
+++ kedro_azureml/auth/utils.py
@@ -1,20 +1,13 @@
 import os
 
 from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
-
-# Make the Azure exception import optional to avoid hard failures when azure.core is absent
-try:
-    from azure.core.exceptions import ClientAuthenticationError  # type: ignore
-except ImportError:  # azure.core not installed or version mismatch
-    class ClientAuthenticationError(Exception):
-        """Fallback exception to allow local/dev environments without azure.core."""
-        pass
+from azure.core.exceptions import ClientAuthenticationError
 
 
 def get_azureml_credentials():
     try:
-        # On an AzureML compute instance, the managed identity will take precedence,
-        # while it may not have enough permissions.
+        # On a AzureML compute instance, the managed identity will take precedence,
+        # while it does not have enough permissions.
         # So, if we are on an AzureML compute instance, we disable the managed identity.
         is_azureml_managed_identity = "MSI_ENDPOINT" in os.environ
         credential = DefaultAzureCredential(
@@ -22,7 +15,7 @@ def get_azureml_credentials():
         )
         # Check if given credential can get token successfully.
         credential.get_token("https://management.azure.com/.default")
-    except (ValueError, ClientAuthenticationError):
-        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential does not work
+    except Exception: # ignore
+        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential not work
         credential = InteractiveBrowserCredential()
     return credential
